a0e109083b075a2bb3e8178db4416fe9bb1a117d
Andrey <andreyvr07@gmail.com>
2024-04-18 17:01:17 -0500

    Avances

--- game-trivia/app.js
+++ game-trivia/app.js
@@ -4,5 +4,14 @@ btnStart.addEventListener('click',(e)=>{
     e.preventDefault()
     document.querySelector('.modal').style.display = 'flex';
     document.querySelector('.container-start').style.display= 'none';
-   
+   viewQuestionModal();
 })
+
+function viewQuestionModal(){
+    const containerModal = document.querySelector('#container-modal')
+    let containerResponse = document.querySelector('#container-response')
+    let titleModal = document.querySelector('.question-number')
+    let descriptionModal = document.querySelector('question')
+    
+    console.log(titleModal)
+}

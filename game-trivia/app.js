a52c110615a652a134b51ccc9c04f4ff5a490d64
Andrey <andreyvr07@gmail.com>
2024-04-22 15:18:21 -0500

    colores

--- game-trivia/app.js
+++ game-trivia/app.js
@@ -1,5 +1,5 @@
 const btnStart = document.querySelector(".button-start");
-let questionsData=[];
+let questionsData = [];
 let count = 0;
 btnStart.addEventListener("click", async (e) => {
   e.preventDefault();
@@ -9,97 +9,89 @@ btnStart.addEventListener("click", async (e) => {
 });
 
 async function createQuestions() {
-    const response = await fetch("./questions.json");
-    let questionsJson = await response.json();
-    
-    for(let i = 0; i<5 ; i++){
-        const indexRandom = Math.floor(Math.random() * questionsJson.length)
-    
-        questionsData.push(questionsJson.splice(indexRandom, 1)[0]);
-    }
-    loadQuestion()
+  const response = await fetch("./questions.json");
+  let questionsJson = await response.json();
+
+  for (let i = 0; i < 5; i++) {
+    const indexRandom = Math.floor(Math.random() * questionsJson.length);
+
+    questionsData.push(questionsJson.splice(indexRandom, 1)[0]);
+  }
+  loadQuestion();
 }
 
 function loadQuestion() {
-    
-  const containerModal = document.querySelector('#container-modal')
+  const containerModal = document.querySelector("#container-modal");
   let containerResponse = document.querySelector("#container-response");
   let titleModal = document.querySelector(".question-number");
   let descriptionModal = document.querySelector(".question");
   const btnAcctions = document.querySelector(".buttons-actions");
 
-  if( count < questionsData.length ){
-       const questions = questionsData[count];
-       containerResponse.innerHTML = "";
-       titleModal.textContent = `Question #${count +1}`;
-       descriptionModal.textContent = questions.question;
-     
-       for (let i = 0; i < questions.options.length; i++) {
-         let responseOptions = document.createElement("div");
-         responseOptions.classList.add("response");
-     
-         responseOptions.innerHTML = `
+  if (count < questionsData.length) {
+    const questions = questionsData[count];
+    containerResponse.innerHTML = "";
+    titleModal.textContent = `Question #${count + 1}`;
+    descriptionModal.textContent = questions.question;
+
+    for (let i = 0; i < questions.options.length; i++) {
+      let responseOptions = document.createElement("div");
+      responseOptions.classList.add("response");
+
+      responseOptions.innerHTML = `
            <input type="radio" class="reponse-input" name="${questions.options[i]}" value="${questions.options[i]}">
            <label for="${questions.options[i]}">${questions.options[i]}</label>
            `;
-         containerResponse.appendChild(responseOptions);
-       }
-     
-       containerModal.append(
-         titleModal,
-         descriptionModal,
-         containerResponse,
-         btnAcctions
-       );
-       validatedOption(questions.number);
+      containerResponse.appendChild(responseOptions);
+    }
 
-  }else{
-    containerModal.innerHTML = '';
-    console.log('No hay mas pa mostrar')
+    containerModal.append(
+      titleModal,
+      descriptionModal,
+      containerResponse,
+      btnAcctions
+    );
+    validatedOption(questions.number);
+  } else {
+    containerModal.innerHTML = "";
+    console.log("No hay mas pa mostrar");
+    const answers = evualateAnswers();
+    console.log(answers);
+
   }
-
 }
 
 const btnNextQuestion = document.querySelector("#btn-next");
 
-btnNextQuestion.addEventListener("click",()=>{
- count++;
-console.log('Sisas', count)
-loadQuestion()
+btnNextQuestion.addEventListener("click", () => {
+  count++;
+  loadQuestion();
 });
 
 let answers = [];
 function validatedOption(numberQuestion) {
-  console.log(numberQuestion)
   const responseOption = document.querySelectorAll(".reponse-input");
-  const containerModal = document.querySelector(".modal");
   let optionSelect;
 
-  for(let i = 0; i < responseOption.length; i++){
-    responseOption[i].addEventListener('click',()=>{
-    console.log(responseOption[i].value);
-    optionSelect = responseOption[i].value;
-    answers.push({number: numberQuestion, answerSelect:optionSelect})
-  })
-}
-evualateAnswers();
-  console.log(answers)
-
-}
-function evualateAnswers(){
-  const correctAnswers = [];
-  const incorrectAnswers = [];
-  for(let i =0 ; i< answers.length; i++){
-
-    if(questionsData[i].correct_answer === answers[i].answerSelect){
-        correctAnswers.push(i)
-    }else{
-       incorrectAnswers.push(i);
-    }
-    
+  for (let i = 0; i < responseOption.length; i++) {
+    responseOption[i].addEventListener("click", () => {
+      console.log(responseOption[i].value);
+      optionSelect = responseOption[i].value;
+      answers.push({ number: numberQuestion, answerSelect: optionSelect });
+    });
   }
-    return {correctAnswers, incorrectAnswers};
-
 }
-  
 
+function evualateAnswers() {
+  let answerCorrect = 0;
+  let incorrectAnswers = 0;
+  console.log(answers)
+  console.log(questionsData);
+  for (let i = 0; i < answers.length; i++) {
+    if (questionsData[i].correct_answer === answers[i].answerSelect) {
+      ++answerCorrect      
+    } else if(questionsData[i].correct_answer !== answers[i].answerSelect){
+      ++incorrectAnswers     
+    }
+  }
+    return {correctAnswers: answerCorrect, incorrectAnswers: incorrectAnswers };
+}

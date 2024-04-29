845de81114309a5bf269f61b562da2f4ec43f767
Andrey <andreyvr07@gmail.com>
2024-04-29 16:55:36 -0500

    Falta el time;

--- deck Random/app.js
+++ deck Random/app.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  await selectMortysRadom();
+  await createCartsMorty();
   createCartsMortys();
 });
 
@@ -8,7 +8,7 @@ const btnPlay = document.querySelector(".container-button-play");
 btnPlay.addEventListener("click", async () => {
   btnPlay.style.display = "none";
   await activeHoverCartsEfeccts();
-  await selectCarts();
+  selectCarts();
 });
 
 // Funcion para agregar los estilos del hover a las cartas
@@ -36,7 +36,7 @@ async function activeHoverCartsEfeccts() {
 // Funcion que me retorna aleatoriamente 6 objetos del json
 
 let dataJson = [];
-async function selectMortysRadom() {
+async function createCartsMorty() {
   const response = await fetch("./mortys.json");
   let data = await response.json();
   for (let i = 0; i < 6; i++) {
@@ -66,51 +66,98 @@ function createCartsMortys() {
   }
 }
 
-let clicks = 0,
-    correct = 0,
-    incorrect = 0;
-
-
-async function selectCarts() {
+// funcion para la seleccion de cartas
+function selectCarts() {
 
   const elements = document.querySelectorAll(".element");
-  let selectMorty = [];
-  let backgroundsSelects = [];
-  let elementsHTML = [];
-  
+  elements.forEach(element => {
+    element.addEventListener("click", () => {
+      handleElementClick(element);
+    });
+  });
+}
+// variables Globales
+let selectMorty = [];
+let backgroundsSelects = [];
+let idElementSelected = [];
+let clicks = 0;
+let correct = 0;
+let incorrect = 0;
 
+// Funcion para manejar el evento click y recorrer los elementos
+ function handleElementClick(element){
+  ++clicks
+  const backgroundImg = element.querySelector(".element-img_background");
+  const mortyImg = element.querySelector('.element-img_morty');
+   backgroundImg.style.display = 'none';
+   mortyImg.style.display = 'flex';
+
+   let cartSelect = mortyImg.alt;
+   let selectedBackground = backgroundImg.alt;
+   let idElementSelect = mortyImg.id;
+   
+ selectMorty.push(cartSelect);
+ backgroundsSelects.push(selectedBackground);
+ idElementSelected.push(idElementSelect);
+ validateSelectdElements(selectMorty, backgroundsSelects, idElementSelected)
+
+}
+
+// Funcion que valida las selecciones
+function validateSelectdElements(selectMorty,backgroundsSelects, idElementSelected ){
+if (selectMorty[1]) {
+//CORRECT SELECT
+if ( selectMorty[0] === selectMorty[1] && backgroundsSelects[0] !== backgroundsSelects[1]) {
+  ++correct;
+  const element1 = document.getElementById(idElementSelected[0]);
+  const element2 = document.getElementById(idElementSelected[1]);
+
+  element1.style.border = "5px solid #0BF845";
+  element2.style.border = "5px solid #0BF845";
+
+  resetSelectsArrays()
+} else {
+  ++incorrect;
+  //FALSE SELECT
+  const element1 = document.getElementById(idElementSelected[0]);
+  const element2 = document.getElementById(idElementSelected[1]);
+
+  const background1 = document.querySelector(`img[alt="${backgroundsSelects[0]}"]`);
+  const background2 = document.querySelector(`img[alt="${backgroundsSelects[1]}"]`);
+
+  element1.style.border = "5px solid #F70909";
+  element2.style.border = "5px solid #F70909";
   
+  setTimeout(() => {
+
+    editStyleDisplay(element1,element2, 'none')
+    editStyleDisplay(background1, background2, 'flex')
+    resetSelectsArrays()
+    
+  }, 1000);
+} 
+validatedScore(clicks, correct, incorrect); 
+}
+}
+
   //funcion para mostrar y ocultar una carta
-  function displayStyle(element1, element2, displayStyle){
+  function editStyleDisplay(element1, element2, displayStyle){
     element1.style.display = displayStyle;
     element2.style.display = displayStyle;
   }
-
  // funcion para limpiar los arreglos
-  function resetSelects(){
+  function resetSelectsArrays(){
     selectMorty = [];
     backgroundsSelects = [];
-    elementsHTML = [];
+    idElementSelected = [];
   }
 
 
-  const btnTryAgain = document.querySelector("#btn-tryagain");
-
-  // button para reinciar
-  btnTryAgain.addEventListener("click", () => {
-    setTimeout(() => {
-      window.location.reload();
-    }, 1000);
-  });
-}
-
-
-
  function validatedScore(clicks = 0, correct = 0, incorrect = 0) {
-   let score = (correct * 10) / clicks;
+   let score = (correct * 10) - clicks;
    score =isNaN(score) ? 0 : score; 
    
-   createElementScore(clicks,score, correct)
+   createElementScore(clicks,score - incorrect, correct)
   }
   
   function createElementScore(clicks,  score, correct){
@@ -128,79 +175,35 @@ async function selectCarts() {
 
         </div>  
         <div class="container-title_button">
-            <button id="btn-tryagain">Try Again</button>
+            <button id="btn-tryagain"  onclick="window.location.reload()">Try Again</button>
         </div>
     </div>
   `;
+  if(correct === 6) {
+    
+    finishGame(score, clicks, incorrect);
+    optionsDivTitle.innerHTML=''
+  }
   containerOptions.append(optionsDivTitle);
 }
 
-
-function handelElementClick(elements){
- elements.forEach(element=>{
-   element.addEventListener("click", () => {
-     ++clicks
-     const backgroundImg =  element.querySelector(".element-img_background").style.display = "none";
-     const mortyImg = element.querySelector('.element-img_morty');
-      backgroundImg.style.display = 'none';
-      mortyImg.style.display = 'flex';
-
-      let cartSelect = mortyImg.alt;
-      let backgroundSelect = backgroundImg.alt;
-      let elementSelect = mortyImg.id;
-      
-    })
- })
-}
-
-elements.forEach((element) => {
-  element.addEventListener("click", () => {
-    ++clicks
-    element.querySelector(".element-img_background").style.display = "none";
-    element.querySelector(".element-img_morty").style.display = "flex";
-
-    let cartSelect = element.querySelector(".element-img_morty").alt;
-    let backgroundSelect = element.querySelector(".element-img_background").alt;
-    let elementSelect = element.querySelector(".element-img_morty").id;
-
-    selectMorty.push(cartSelect);
-    backgroundsSelects.push(backgroundSelect);
-    elementsHTML.push(elementSelect);
-
-    if (selectMorty[1]) {
-      //CORRECT SELECT
-      if ( selectMorty[0] === selectMorty[1] && backgroundsSelects[0] !== backgroundsSelects[1]) {
-        ++correct;
-        const element1 = document.getElementById(elementsHTML[0]);
-        const element2 = document.getElementById(elementsHTML[1]);
-
-        element1.style.border = "5px solid #0BF845";
-        element2.style.border = "5px solid #0BF845";
-
-        resetSelects()
-      } else {
-        ++incorrect;
-        //FALSE SELECT
-        const element1 = document.getElementById(elementsHTML[0]);
-        const element2 = document.getElementById(elementsHTML[1]);
-
-        const background1 = document.querySelector(`img[alt="${backgroundsSelects[0]}"]`);
-        const background2 = document.querySelector(`img[alt="${backgroundsSelects[1]}"]`);
-
-        element1.style.border = "5px solid #F70909";
-        element2.style.border = "5px solid #F70909";
-        
-        setTimeout(() => {
-
-          displayStyle(element1,element2, 'none')
-          displayStyle(background1, background2, 'flex')
-          resetSelects()
-          
-        }, 1000);
-      } 
-      validatedScore(clicks, correct, incorrect); 
-    }
-  });
+function finishGame(score, clicks, incorrect, time){
+  const container = document.querySelector('.container-carts');
+  const containerFinish  = document.createElement('div')
+  const titleGame = document.querySelector('.title-game');
   
- 
-});
+  containerFinish.classList.add('container-finish');
+
+  titleGame.innerHTML = '';
+  container.innerHTML = '';
+  containerFinish.innerHTML = `
+  <h1 style="color:#09f; font-size:50px; margin-bottom: 15px;"> Congratulations Has Finish<b style="color:#fff;"> Morty Game</b></h1>
+  <h2>Score: <b style="color:#09f;"> ${score}</b></h2>
+  <h2>Clicks:<b style="color:#09f;"> ${clicks}</b></h2>
+  <h2>Errores:<b style="color:#09f;"> ${incorrect}</b></h2>
+  <h2>Time: <b style="color:#09f;">${time}</b></h2>
+
+  <button class="btn-finish" onclick="window.location.reload()"> Play Again </button>
+  `
+  container.appendChild(containerFinish)
+}
